06c455a04abc768e9ea68bb7ef59bf1d53868b17
Anandsai2604 <154312908+Anandsai2604@users.noreply.github.com>
2024-07-16 23:03:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import Front1 from './comp/front1';
 import IPL from './comp/ipl';
 import WorldCup from './comp/worldcup';
 import Stats from './comp/Stats6';
+import LoginRegister from './comp/loginRegister'; 
 import Navigation from './comp/Navigation';
-import Rag from './comp/rag';
-// import Front from './comp/front'
+import PrivateRoutes from './comp/PrivateRoutes'; 
+import Front from './comp/front';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        
         <Navigation />
         <Routes>
-          <Route path="/" element={<Front1 />} />
-          <Route path="/ipl" element={<IPL />} />
-          <Route path="/worldcup" element={<WorldCup />} />
-          <Route path="/stats" element={<Stats />} />
-          <Route path='/rag' element={<Rag/>}/>
-          {/* <Route path='/front' element={<Front/>}/> */}
+          <Route path="/" element={<LoginRegister />} />
+          <Route path="/loginRegister" element={<LoginRegister />} />
+
+          <Route element={<PrivateRoutes />}>
+            <Route path="/front1/:userId" element={<Front1 />} />
+            <Route path="/ipl" element={<IPL />} />
+            <Route path="/worldcup" element={<WorldCup />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/front/:userId" element={<Front />} />
+          </Route>
+          
         </Routes>
       </div>
     </BrowserRouter>
